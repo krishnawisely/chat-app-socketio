514aa90c2ecf8f404a2355510dcb6a31cd8ac8f0
Krishnamoorthy <krishna192168@gmail.com>
2022-10-13 06:21:53 +0000

    updated script

--- post-commit-command.sh
+++ post-commit-command.sh
@@ -4,7 +4,7 @@ cat <<EOF >.git/hooks/post-commit
 #!/bin/sh
   
 # Get log and store it into text file
-git log --name-status HEAD^..HEAD > /workspace/chat-app-socketio/history_log.txt
+git log -1 --shortstat > /workspace/chat-app-socketio/history_log.txt
 
 # Send store file to RevPro
 curl -X POST --data "@history_log.txt" "https://webhook.site/0aff6c9e-f361-46c8-9d74-9dd5f2ec08ff"
